858364799385369953274fcbba68d5567f9654f1
Hemanth K <hemanthhemik2003@gmail.com>
2024-09-26 18:51:31 +0530

    task10 is added by hemanth

--- task8.py
+++ task8.py
@@ -1,7 +1,6 @@
 def is_armstrong(n):
     digits = str(n)
     num_digits = len(digits)
-    
     sum_of_powers = sum(int(digit) ** num_digits for digit in digits)
     return sum_of_powers == n
 if is_armstrong(number):
